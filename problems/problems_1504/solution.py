2fac9e2b2a75c5d707c285f04bd973bbfd88b293
GitHub Action <action@github.com>
2025-08-20 16:00:48 +0000

    test: [20250821] Add (1504)

---
+++ problems/problems_1504/solution.py
@@ -0,0 +1,11 @@
+import solution
+from typing import *
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        return self.numSubmat(test_input)
+
+    def numSubmat(self, mat: List[List[int]]) -> int:
+        pass
+

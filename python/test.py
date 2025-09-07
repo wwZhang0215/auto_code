172b10df0178eae37383457d74fec8ae9d5cb2cf
GitHub Action <action@github.com>
2025-09-07 16:00:58 +0000

    test: [20250908] Add (1317)

--- python/test.py
+++ python/test.py
@@ -11,7 +11,7 @@ from utils import get_default_folder, timeout
 logging.basicConfig(level=logging.INFO, format=constants.LOGGING_FORMAT, datefmt=constants.DATE_FORMAT)
 
 # Question ID that wants to test, modify here as passing arguments
-QUESTION = "1304"
+QUESTION = "1317"
 # QUESTION = "Interview/10_02"
 # QUESTION = "LCP/07"
 # QUESTION = "剑指Offer/52"

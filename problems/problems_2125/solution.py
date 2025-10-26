56ca400340c36e00264194800d15e753264cc527
GitHub Action <action@github.com>
2025-10-26 16:01:10 +0000

    test: [20251027] Add (2125)

---
+++ problems/problems_2125/solution.py
@@ -0,0 +1,11 @@
+import solution
+from typing import *
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        return self.numberOfBeams(test_input)
+
+    def numberOfBeams(self, bank: List[str]) -> int:
+        pass
+

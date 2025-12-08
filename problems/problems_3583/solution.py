46968bf38384c88140f6326f1e09f6311aacb4a9
GitHub Action <action@github.com>
2025-12-08 16:01:05 +0000

    test: [20251209] Add (3583)

---
+++ problems/problems_3583/solution.py
@@ -0,0 +1,11 @@
+import solution
+from typing import *
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        return self.specialTriplets(test_input)
+
+    def specialTriplets(self, nums: List[int]) -> int:
+        pass
+

75c32baacd025c39b0dae36be2796886962f6773
GitHub Action <action@github.com>
2025-12-10 16:01:24 +0000

    test: [20251211] Add (3531)

---
+++ problems/problems_3531/solution.py
@@ -0,0 +1,11 @@
+import solution
+from typing import *
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        return self.countCoveredBuildings(*test_input)
+
+    def countCoveredBuildings(self, n: int, buildings: List[List[int]]) -> int:
+        pass
+

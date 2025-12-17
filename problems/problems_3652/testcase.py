297bfbde5ca10c864895631b717dd334de9e74ae
GitHub Action <action@github.com>
2025-12-17 16:01:23 +0000

    test: [20251218] Add (3652)

---
+++ problems/problems_3652/testcase.py
@@ -0,0 +1,14 @@
+from collections import namedtuple
+import testcase
+
+case = namedtuple("Testcase", ["Input", "Output"])
+
+
+class Testcase(testcase.Testcase):
+	def __init__(self):
+		self.testcases = []
+		self.testcases.append(case(Input=[[4, 2, 8], [-1, 0, 1], 2], Output=10))
+		self.testcases.append(case(Input=[[5, 4, 3], [1, 1, 0], 2], Output=9))
+
+	def get_testcases(self):
+		return self.testcases

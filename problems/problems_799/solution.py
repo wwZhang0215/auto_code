eb44741a3bb8f7df1de49cae597ab5e77bdda70f
GitHub Action <action@github.com>
2026-02-13 16:10:35 +0000

    test: [20260214] Add (799)

---
+++ problems/problems_799/solution.py
@@ -0,0 +1,11 @@
+import solution
+from typing import *
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        return self.champagneTower(*test_input)
+
+    def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
+        pass
+

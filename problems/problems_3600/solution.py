8b43bb80de4f48e97794d7409460b70dfa1568d8
GitHub Action <action@github.com>
2026-03-11 16:12:37 +0000

    test: [20260312] Add (3600)

---
+++ problems/problems_3600/solution.py
@@ -0,0 +1,11 @@
+import solution
+from typing import *
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        return self.maxStability(*test_input)
+
+    def maxStability(self, n: int, edges: List[List[int]], k: int) -> int:
+        pass
+

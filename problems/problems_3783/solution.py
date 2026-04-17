9802638bde018a5583410eb7bd828e194847456a
GitHub Action <action@github.com>
2026-04-17 16:16:23 +0000

    test: [20260418] Add (3783)

---
+++ problems/problems_3783/solution.py
@@ -0,0 +1,11 @@
+import solution
+from typing import *
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        return self.mirrorDistance(test_input)
+
+    def mirrorDistance(self, n: int) -> int:
+        pass
+

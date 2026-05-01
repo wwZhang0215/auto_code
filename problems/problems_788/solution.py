50c3848a5d6fb5ff26c4b09ef5165e19e6296d8b
GitHub Action <action@github.com>
2026-05-01 16:15:56 +0000

    test: [20260502] Add (788)

---
+++ problems/problems_788/solution.py
@@ -0,0 +1,11 @@
+import solution
+from typing import *
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        return self.rotatedDigits(test_input)
+
+    def rotatedDigits(self, n: int) -> int:
+        pass
+

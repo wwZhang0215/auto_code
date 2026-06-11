ece80dcb3826e3a2b58eab00dcb0f771b025f6ae
GitHub Action <action@github.com>
2026-06-11 17:07:58 +0000

    test: [20260612] Add (3559)

---
+++ problems/problems_3559/solution.py
@@ -0,0 +1,11 @@
+import solution
+from typing import *
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        return self.assignEdgeWeights(*test_input)
+
+    def assignEdgeWeights(self, edges: List[List[int]], queries: List[List[int]]) -> List[int]:
+        pass
+

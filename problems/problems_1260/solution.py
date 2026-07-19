c5c0a0d65e3d1f819ced3db73bfef8f4dcbddd2a
GitHub Action <action@github.com>
2026-07-19 16:39:38 +0000

    test: [20260720] Add (1260)

---
+++ problems/problems_1260/solution.py
@@ -0,0 +1,11 @@
+import solution
+from typing import *
+
+
+class Solution(solution.Solution):
+    def solve(self, test_input=None):
+        return self.shiftGrid(*test_input)
+
+    def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
+        pass
+
